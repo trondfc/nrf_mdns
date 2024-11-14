f3dcbf4767bea46c40ac631b660a068f68442ea2
Trondfc <trochr@hotmail.com>
2024-11-14 09:39:54 +0100

    Add initial configuration and setup for Wi-Fi mDNS responder and resolver

---
+++ sta/Readme.md
@@ -0,0 +1,30 @@
+## Setup
+WiFi ssid and password are set in `prj.conf` for both the responder and resolver.
+Responder hostname is set in `overlay-responder.conf` and resolver query in `overlay-resolver.conf`. These two should be the same.
+After flashing the devices, the devices should be connected to the same network. For testing purposes the mDNS queries is sent after pushing button 1 on the resolver. 
+
+## Building
+### Responder
+The device responding to local mndns queries. 
+```
+west build -p -b nrf5340_audio_dk/nrf5340/cpuapp -d build_responder --sysbuild -- -DSHIELD=nrf7002ek  -DEXTRA_CONF_FILE="overlay-responder.conf"
+
+west flash -d build_responder
+```
+
+
+### Resolver
+The device 
+```
+west build -p -b nrf5340_audio_dk/nrf5340/cpuapp -d build_resolver --sysbuild -- -DSHIELD=nrf7002ek  -DEXTRA_CONF_FILE="overlay-resolver.conf"
+
+west flash -d build_resolver
+```
+
+## TODO
+- Zephyr mDNS has had a rewrite allowing for responder and resolver to be in the same application [github](https://github.com/zephyrproject-rtos/zephyr/pull/73422). This change is not yet in ncs sdk 2.7.0 (using sdk-zephyr v3.6.99) but should be in 2.8.0 (using sdk-zephyr v3.7.99)
+
+## Sources
+- [nrf mDNS responder](https://docs.nordicsemi.com/bundle/ncs-2.7.0/page/zephyr/samples/net/mdns_responder/README.html)
+- [nrf mDNS resolver](https://docs.nordicsemi.com/bundle/ncs-2.1.1/page/zephyr/samples/net/dns_resolve/README.html#dns-resolve-sample)
+- [nrf wifi station](https://docs.nordicsemi.com/bundle/ncs-latest/page/nrf/samples/wifi/sta/README.html)
---
+++ sta/src/main.c
@@ -0,0 +1,526 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ * @brief WiFi station sample
+ */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(sta, CONFIG_LOG_DEFAULT_LEVEL);
+
+#include <nrfx_clock.h>
+#include <zephyr/kernel.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/init.h>
+
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/net_event.h>
+#include <zephyr/drivers/gpio.h>
+
+#include <net/wifi_mgmt_ext.h>
+#include <net/wifi_ready.h>
+
+#include <qspi_if.h>
+
+#include "net_private.h"
+
+#include <dk_buttons_and_leds.h>
+
+#include <zephyr/net/dns_resolve.h>
+
+#define DNS_TIMEOUT (10 * MSEC_PER_SEC)
+
+#define WIFI_SHELL_MODULE "wifi"
+
+#define WIFI_SHELL_MGMT_EVENTS (NET_EVENT_WIFI_CONNECT_RESULT |		\
+				NET_EVENT_WIFI_DISCONNECT_RESULT)
+
+#define MAX_SSID_LEN        32
+#define STATUS_POLLING_MS   300
+
+/* 1000 msec = 1 sec */
+#define LED_SLEEP_TIME_MS   100
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+static struct net_mgmt_event_callback wifi_shell_mgmt_cb;
+static struct net_mgmt_event_callback net_shell_mgmt_cb;
+
+#ifdef CONFIG_WIFI_READY_LIB
+static K_SEM_DEFINE(wifi_ready_state_changed_sem, 0, 1);
+static bool wifi_ready_status;
+#endif /* CONFIG_WIFI_READY_LIB */
+
+static struct {
+	const struct shell *sh;
+	union {
+		struct {
+			uint8_t connected	: 1;
+			uint8_t connect_result	: 1;
+			uint8_t disconnect_requested	: 1;
+			uint8_t _unused		: 5;
+		};
+		uint8_t all;
+	};
+} context;
+
+#ifdef CONFIG_MDNS_MODE_RESOLVER
+void mdns_result_cb(enum dns_resolve_status status,
+				struct dns_addrinfo *info,
+				void *user_data) {
+
+	char hr_addr[NET_IPV6_ADDR_LEN];
+	char *hr_family;
+	void *addr;
+
+	LOG_WRN("mDNS resolving status: %d", status);
+	switch (status) {
+	case DNS_EAI_CANCELED:
+		LOG_WRN("DNS query canceled");
+		break;
+	case DNS_EAI_FAIL:
+		LOG_WRN("DNS query failed");
+		break;
+	case DNS_EAI_NODATA:
+		LOG_WRN("DNS query returned no data");
+		break;
+	case DNS_EAI_ALLDONE:
+		LOG_INF("DNS query completed");
+		break;
+	case DNS_EAI_INPROGRESS:
+		LOG_INF("DNS query in progress");
+		break;
+	default:
+		LOG_INF("mDNS resolving error: %d", status);
+		return;
+	}
+
+	if(!info) {
+		LOG_WRN("No info");
+		return;
+	}
+
+	if (info->ai_family == AF_INET) {
+		hr_family = "IPv4";
+		addr = &net_sin(&info->ai_addr)->sin_addr;
+	} else if (info->ai_family == AF_INET6) {
+		hr_family = "IPv6";
+		addr = &net_sin6(&info->ai_addr)->sin6_addr;
+	} else {
+		LOG_ERR("Invalid IP address family %d", info->ai_family);
+		return;
+	}
+
+	LOG_INF("%s %s address: %s", user_data ? (char *)user_data : "<null>",
+		hr_family,
+		net_addr_ntop(info->ai_family, addr,
+					 hr_addr, sizeof(hr_addr)));
+}
+
+static void do_mdns_query(void)
+{
+	// static const char *mdns_query = "wifi-gateway.local";
+	char* mdns_query = malloc(strlen(CONFIG_MDNS_QUERY_NAME)+strlen(".local")+1);
+	strcpy(mdns_query, CONFIG_MDNS_QUERY_NAME);
+	strcat(mdns_query, ".local");
+
+	int ret;
+
+	LOG_WRN("Starting mDNS query for %s", mdns_query);
+
+	// Change the query type to DNS_QUERY_TYPE_AAAA for IPv6
+	ret = dns_get_addr_info(mdns_query,
+				DNS_QUERY_TYPE_A,
+				NULL,
+				mdns_result_cb,
+				(void *)mdns_query,
+				DNS_TIMEOUT);
+	if (ret < 0) {
+		LOG_ERR("Cannot resolve mDNS IPv4 address (%d)", ret);
+		return;
+	}
+
+	LOG_WRN("mDNS v4 query sent");
+
+}
+
+#endif /* CONFIG_MDNS_MODE_RESOLVER */
+static void button_handler(uint32_t button_state, uint32_t has_changed){
+	uint32_t buttons = button_state & has_changed;
+	if(buttons & DK_BTN1_MSK){
+		LOG_INF("Button 1 pressed");
+		#ifdef CONFIG_MDNS_MODE_RESOLVER
+		do_mdns_query();
+		#endif
+	}
+	if(buttons & DK_BTN2_MSK){
+		LOG_INF("Button 2 pressed");
+	}
+	if(buttons & DK_BTN3_MSK){
+		LOG_INF("Button 3 pressed");
+	}
+	if(buttons & DK_BTN4_MSK){
+		LOG_INF("Button 4 pressed");
+	}
+}
+
+void toggle_led(void)
+{
+	int ret;
+
+	if (!device_is_ready(led.port)) {
+		LOG_ERR("LED device is not ready");
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		LOG_ERR("Error %d: failed to configure LED pin", ret);
+		return;
+	}
+
+	while (1) {
+		if (context.connected) {
+			gpio_pin_toggle_dt(&led);
+			k_msleep(LED_SLEEP_TIME_MS);
+		} else {
+			gpio_pin_set_dt(&led, 0);
+			k_msleep(LED_SLEEP_TIME_MS);
+		}
+	}
+}
+
+K_THREAD_DEFINE(led_thread_id, 1024, toggle_led, NULL, NULL, NULL,
+		7, 0, 0);
+
+static int cmd_wifi_status(void)
+{
+	struct net_if *iface = net_if_get_default();
+	struct wifi_iface_status status = { 0 };
+
+	if (net_mgmt(NET_REQUEST_WIFI_IFACE_STATUS, iface, &status,
+				sizeof(struct wifi_iface_status))) {
+		LOG_INF("Status request failed");
+
+		return -ENOEXEC;
+	}
+
+	LOG_INF("==================");
+	LOG_INF("State: %s", wifi_state_txt(status.state));
+
+	if (status.state >= WIFI_STATE_ASSOCIATED) {
+		uint8_t mac_string_buf[sizeof("xx:xx:xx:xx:xx:xx")];
+
+		LOG_INF("Interface Mode: %s",
+		       wifi_mode_txt(status.iface_mode));
+		LOG_INF("Link Mode: %s",
+		       wifi_link_mode_txt(status.link_mode));
+		LOG_INF("SSID: %.32s", status.ssid);
+		LOG_INF("BSSID: %s",
+		       net_sprint_ll_addr_buf(
+				status.bssid, WIFI_MAC_ADDR_LEN,
+				mac_string_buf, sizeof(mac_string_buf)));
+		LOG_INF("Band: %s", wifi_band_txt(status.band));
+		LOG_INF("Channel: %d", status.channel);
+		LOG_INF("Security: %s", wifi_security_txt(status.security));
+		LOG_INF("MFP: %s", wifi_mfp_txt(status.mfp));
+		LOG_INF("RSSI: %d", status.rssi);
+	}
+	return 0;
+}
+
+static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb)
+{
+	const struct wifi_status *status =
+		(const struct wifi_status *) cb->info;
+
+	if (context.connected) {
+		return;
+	}
+
+	if (status->status) {
+		LOG_ERR("Connection failed (%d)", status->status);
+	} else {
+		LOG_INF("Connected");
+		context.connected = true;
+	}
+
+	context.connect_result = true;
+}
+
+static void handle_wifi_disconnect_result(struct net_mgmt_event_callback *cb)
+{
+	const struct wifi_status *status =
+		(const struct wifi_status *) cb->info;
+
+	if (!context.connected) {
+		return;
+	}
+
+	if (context.disconnect_requested) {
+		LOG_INF("Disconnection request %s (%d)",
+			 status->status ? "failed" : "done",
+					status->status);
+		context.disconnect_requested = false;
+	} else {
+		LOG_INF("Received Disconnected");
+		context.connected = false;
+	}
+
+	cmd_wifi_status();
+}
+
+static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
+				     uint32_t mgmt_event, struct net_if *iface)
+{
+	switch (mgmt_event) {
+	case NET_EVENT_WIFI_CONNECT_RESULT:
+		handle_wifi_connect_result(cb);
+		break;
+	case NET_EVENT_WIFI_DISCONNECT_RESULT:
+		handle_wifi_disconnect_result(cb);
+		break;
+	default:
+		break;
+	}
+}
+
+static void print_dhcp_ip(struct net_mgmt_event_callback *cb)
+{
+	/* Get DHCP info from struct net_if_dhcpv4 and print */
+	const struct net_if_dhcpv4 *dhcpv4 = cb->info;
+	const struct in_addr *addr = &dhcpv4->requested_ip;
+	char dhcp_info[128];
+
+	net_addr_ntop(AF_INET, addr, dhcp_info, sizeof(dhcp_info));
+
+	LOG_INF("DHCP IP address: %s", dhcp_info);
+}
+static void net_mgmt_event_handler(struct net_mgmt_event_callback *cb,
+				    uint32_t mgmt_event, struct net_if *iface)
+{
+	switch (mgmt_event) {
+	case NET_EVENT_IPV4_DHCP_BOUND:
+		print_dhcp_ip(cb);
+		break;
+	default:
+		break;
+	}
+}
+
+static int wifi_connect(void)
+{
+	struct net_if *iface = net_if_get_first_wifi();
+
+	context.connected = false;
+	context.connect_result = false;
+
+	if (net_mgmt(NET_REQUEST_WIFI_CONNECT_STORED, iface, NULL, 0)) {
+		LOG_ERR("Connection request failed");
+
+		return -ENOEXEC;
+	}
+
+	LOG_INF("Connection requested");
+
+	return 0;
+}
+
+int bytes_from_str(const char *str, uint8_t *bytes, size_t bytes_len)
+{
+	size_t i;
+	char byte_str[3];
+
+	if (strlen(str) != bytes_len * 2) {
+		LOG_ERR("Invalid string length: %zu (expected: %d)\n",
+			strlen(str), bytes_len * 2);
+		return -EINVAL;
+	}
+
+	for (i = 0; i < bytes_len; i++) {
+		memcpy(byte_str, str + i * 2, 2);
+		byte_str[2] = '\0';
+		bytes[i] = strtol(byte_str, NULL, 16);
+	}
+
+	return 0;
+}
+
+int start_app(void)
+{
+#if defined(CONFIG_BOARD_NRF7002DK_NRF7001_NRF5340_CPUAPP) || \
+	defined(CONFIG_BOARD_NRF7002DK_NRF5340_CPUAPP)
+	if (strlen(CONFIG_NRF700X_QSPI_ENCRYPTION_KEY)) {
+		int ret;
+		char key[QSPI_KEY_LEN_BYTES];
+
+		ret = bytes_from_str(CONFIG_NRF700X_QSPI_ENCRYPTION_KEY, key, sizeof(key));
+		if (ret) {
+			LOG_ERR("Failed to parse encryption key: %d\n", ret);
+			return 0;
+		}
+
+		LOG_DBG("QSPI Encryption key: ");
+		for (int i = 0; i < QSPI_KEY_LEN_BYTES; i++) {
+			LOG_DBG("%02x", key[i]);
+		}
+		LOG_DBG("\n");
+
+		ret = qspi_enable_encryption(key);
+		if (ret) {
+			LOG_ERR("Failed to enable encryption: %d\n", ret);
+			return 0;
+		}
+		LOG_INF("QSPI Encryption enabled");
+	} else {
+		LOG_INF("QSPI Encryption disabled");
+	}
+#endif /* CONFIG_BOARD_NRF700XDK_NRF5340 */
+
+	LOG_INF("Static IP address (overridable): %s/%s -> %s",
+		CONFIG_NET_CONFIG_MY_IPV4_ADDR,
+		CONFIG_NET_CONFIG_MY_IPV4_NETMASK,
+		CONFIG_NET_CONFIG_MY_IPV4_GW);
+
+	while (1) {
+#ifdef CONFIG_WIFI_READY_LIB
+		int ret;
+
+		LOG_INF("Waiting for Wi-Fi to be ready");
+		ret = k_sem_take(&wifi_ready_state_changed_sem, K_FOREVER);
+		if (ret) {
+			LOG_ERR("Failed to take semaphore: %d", ret);
+			return ret;
+		}
+
+check_wifi_ready:
+		if (!wifi_ready_status) {
+			LOG_INF("Wi-Fi is not ready");
+			/* Perform any cleanup and stop using Wi-Fi and wait for
+			 * Wi-Fi to be ready
+			 */
+			continue;
+		}
+#endif /* CONFIG_WIFI_READY_LIB */
+		wifi_connect();
+
+		while (!context.connect_result) {
+			cmd_wifi_status();
+			k_sleep(K_MSEC(STATUS_POLLING_MS));
+		}
+
+		if (context.connected) {
+			cmd_wifi_status();
+#ifdef CONFIG_WIFI_READY_LIB
+			ret = k_sem_take(&wifi_ready_state_changed_sem, K_FOREVER);
+			if (ret) {
+				LOG_ERR("Failed to take semaphore: %d", ret);
+				return ret;
+			}
+			goto check_wifi_ready;
+#else
+			k_sleep(K_FOREVER);
+#endif /* CONFIG_WIFI_READY_LIB */
+		}
+	}
+
+	return 0;
+}
+
+#ifdef CONFIG_WIFI_READY_LIB
+void start_wifi_thread(void);
+#define THREAD_PRIORITY K_PRIO_COOP(CONFIG_NUM_COOP_PRIORITIES - 1)
+K_THREAD_DEFINE(start_wifi_thread_id, CONFIG_STA_SAMPLE_START_WIFI_THREAD_STACK_SIZE,
+		start_wifi_thread, NULL, NULL, NULL,
+		THREAD_PRIORITY, 0, -1);
+
+void start_wifi_thread(void)
+{
+	start_app();
+}
+
+void wifi_ready_cb(bool wifi_ready)
+{
+	LOG_DBG("Is Wi-Fi ready?: %s", wifi_ready ? "yes" : "no");
+	wifi_ready_status = wifi_ready;
+	k_sem_give(&wifi_ready_state_changed_sem);
+}
+#endif /* CONFIG_WIFI_READY_LIB */
+
+void net_mgmt_callback_init(void)
+{
+	memset(&context, 0, sizeof(context));
+
+	net_mgmt_init_event_callback(&wifi_shell_mgmt_cb,
+				     wifi_mgmt_event_handler,
+				     WIFI_SHELL_MGMT_EVENTS);
+
+	net_mgmt_add_event_callback(&wifi_shell_mgmt_cb);
+
+	net_mgmt_init_event_callback(&net_shell_mgmt_cb,
+				     net_mgmt_event_handler,
+				     NET_EVENT_IPV4_DHCP_BOUND);
+
+	net_mgmt_add_event_callback(&net_shell_mgmt_cb);
+
+	LOG_INF("Starting %s with CPU frequency: %d MHz", CONFIG_BOARD, SystemCoreClock/MHZ(1));
+	k_sleep(K_SECONDS(1));
+}
+
+#ifdef CONFIG_WIFI_READY_LIB
+static int register_wifi_ready(void)
+{
+	int ret = 0;
+	wifi_ready_callback_t cb;
+	struct net_if *iface = net_if_get_first_wifi();
+
+	if (!iface) {
+		LOG_ERR("Failed to get Wi-Fi interface");
+		return -1;
+	}
+
+	cb.wifi_ready_cb = wifi_ready_cb;
+
+	LOG_DBG("Registering Wi-Fi ready callbacks");
+	ret = register_wifi_ready_callback(cb, iface);
+	if (ret) {
+		LOG_ERR("Failed to register Wi-Fi ready callbacks %s", strerror(ret));
+		return ret;
+	}
+
+	return ret;
+}
+#endif /* CONFIG_WIFI_READY_LIB */
+
+int main(void)
+{
+	int ret = 0;
+
+	ret = dk_buttons_init(button_handler);
+
+	net_mgmt_callback_init();
+
+#ifdef CONFIG_WIFI_READY_LIB
+	ret = register_wifi_ready();
+	if (ret) {
+		return ret;
+	}
+	k_thread_start(start_wifi_thread_id);
+#else
+	start_app();
+#endif /* CONFIG_WIFI_READY_LIB */
+	return ret;
+}
